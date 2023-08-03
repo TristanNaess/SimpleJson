b8330383ed1786a20ce858cd600085b8c780de14
Tristan Naess <tristannaess@gmail.com>
2023-08-02 18:57:03 -0700

    Re-pushing some files due to a bad commit, Added tests for parsing functions and error type, added const to parsing functions, added commented accessors to Json class

--- include/json.hpp
+++ include/json.hpp
@@ -2,8 +2,9 @@
 #define __JSON_HPP__
 
 #include <string>
+#include <string_view> // c++17
 
-namespace Json
+namespace json
 {
     enum class Type
     {
@@ -21,7 +22,41 @@ namespace Json
             Json();
             Json(const std::string& line);
             
+            // for now use a separate getter for each type
+            // all throw if wrong type, bad key or bad index
 
+            // later change to templated version so:
+            //    Number can convert to any numeric, flooring if necessary
+            //    Bool, Number, Null can all convert to string if necessary
+
+            // all come in matching object and array access versions
+            // ModifyingAccessorType getObject(std::string_view key);
+            // ModifyingAccessorType getObject(std::size_t index);
+
+            // ModifyingAccessorType getArray(std::string_view key);
+            // ModifyingAccessorType getArray(std::size_t index);
+
+            // std::string_view getString(std::string_view key) const;
+            // std::string_view getString(std::size_t index) const;
+            // ModifyingAccessorType getString(std::string_view key);
+            // ModifyingAccessorType getString(std::size_t index);
+
+            // double getDouble(std::string_view key) const;
+            // double getDouble(std::size_t index) const;
+            // ModifyingAccessorType getDouble(std::string_view key);
+            // ModifyingAccessorType getDouble(std::size_t index);
+
+            // bool getBool(std::string_view key) const;
+            // bool getBool(std::size_t index) const;
+            // ModifyingAccessorType getBool(std::string_view key) const;
+            // ModifyingAccessorType getBool(std::size_t index) const;
+
+            bool isNull(std::string_view key) const;
+            bool isNull(std::size_t index) const;
+
+            bool containsKey(std::string_view key) const;
+            std::vector<std::string> getKeys() const;
+            std::size_t size() const;
 
         private:
             std::string m_contents;
--- include/parsing.hpp
+++ include/parsing.hpp
@@ -2,17 +2,23 @@
 #define __PARSING_HPP__
 
 #include <string>
-//#include <string_view> // may use if static strings accessed
+#include <utility>
 
 /*
    Functions for locating delimited data within JSON structure
 */
 
-// Given initial quote, return's ending quote location or throws error
-std::size_t match_quote(const std::string& line, std::size_t index);
+// Given initial quote, return's ending quote location or throws json::parsing_error
+std::size_t match_quote(const std::string& line, std::size_t index) const;
 
-// Given opening bracket/brace, finds matching closing bracket/brace or throws error
-std::size_t match_bracket(const std::string& line, std::size_t index);
+// Given opening bracket/brace, finds matching closing bracket/brace or throws json::parsing_error
+// Matches '{' or '[' only
+std::size_t match_bracket(const std::string& line, std::size_t index) const;
+
+// find beginning and end of next field, searching from index to end
+// throws if no whole field after index
+std::pair<std::size_t, std::size_t> next_field(const std::string& line, std::size_t index) const;
+
 
 // Possibly want to add functions for finding the start of the next field in object or array
 
