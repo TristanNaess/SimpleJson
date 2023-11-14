621fc6dfb8122c7f7c6f10d4831a1fd0fd77f6d7
Tristan Naess <tristannaess@gmail.com>
2023-11-13 19:44:32 -0800

    Moved verification functions to verification.hpp/cpp files

---
+++ include/verification.hpp
@@ -0,0 +1,7 @@
+#ifndef __VERIFICATION_HPP__
+#define __VERIFICATION_HPP__
+
+// verifies a passed string is valid JSON
+bool verify_json(const std::string_view line);
+
+#endif//__VERIFICATION_HPP__
