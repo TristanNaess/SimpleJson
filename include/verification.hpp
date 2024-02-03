da588c7c9164a766178d338c26f5f1b9870efeef
Tristan Naess <tristannaess@gmail.com>
2024-02-02 22:32:58 -0800

    verify_*() functions added to verification.hpp because needed for insertion

--- include/verification.hpp
+++ include/verification.hpp
@@ -7,4 +7,11 @@
 // verifies a passed string is valid JSON
 json::Result verify_json(std::string_view line);
 
+json::Result verify_object(std::string_view line);
+json::Result verify_array(std::string_view line);
+json::Result verify_number(std::string_view line);
+json::Result verify_string(std::string_view line);
+json::Result verify_bool(std::string_view line);
+json::Result verify_null(std::string_view line);
+
 #endif//__VERIFICATION_HPP__
--- source/verification.cpp
+++ source/verification.cpp
@@ -9,8 +9,58 @@
 // ALL VERIFICATION FUNCTIONS ASSUME THE STRINGS HAVE WHITESPACE REMOVED
 // ----------------------------------------------------------------------
 
-// verify_*() functions are only called through verify_json() and so are not tested directly
-// data bounds checks and the like are handled by the calling function, either verify_object, array, or json
+// data bounds checks and the like are handled by the calling function
+
+json::Result verify_json(std::string_view line)
+{
+    if (line.size() == 0) return json::Result{std::string("Error verifying json. Empty string")};
+    json::Result res;
+    switch (line.front())
+    {
+        case '{':
+            // check bounds characters in verify_object
+            res = verify_object(line);
+            break;
+        case '[':
+            res = verify_array(line);
+            break;
+        default:
+            // other types as base data not supported. May add, but probably not
+            return json::Result(std::string("Error verifying json. Not object or array data: ") + std::string(line));
+    }
+
+    if (!res)
+    {
+        return json::Result("Error verifying object in json: ", res);
+    }
+    return res;
+}
+
+// TODO: re-implement later with same functions as used to extract fields
+json::Result verify_object(std::string_view line)
+{
+    // initial '{' is verified before call
+    if (line.back() != '}') return json::Result{"Error verifying object. No closing '}': " + std::string(line)};
+    
+
+    // crop off surrounding {}
+    line.remove_prefix(1);
+    line.remove_suffix(1);
+
+    if (line.size() == 0) return json::Result();
+
+    std::cout << '\'' << line << "'\n";
+
+    // Figure out the best way to iterate over fields
+    // Split at ':' and check key and val
+
+    return json::Result{"Not fully verified"};
+}
+
+json::Result verify_array(std::string_view line)
+{
+    return json::Result{ "verify_array() not implemented" };
+}
 
 json::Result verify_number(std::string_view line)
 {
@@ -56,55 +106,4 @@ json::Result verify_bool(std::string_view line)
 json::Result verify_null(std::string_view line)
 {
     return json::Result{ "verify_null() not implemented" };
-}
-
-// TODO: re-implement later with same functions as used to extract fields
-json::Result verify_object(std::string_view line)
-{
-    // initial '{' is verified before call
-    if (line.back() != '}') return json::Result{"Error verifying object. No closing '}': " + std::string(line)};
-    
-
-    // crop off surrounding {}
-    line.remove_prefix(1);
-    line.remove_suffix(1);
-
-    if (line.size() == 0) return json::Result();
-
-    std::cout << '\'' << line << "'\n";
-
-    // Figure out the best way to iterate over fields
-    // Split at ':' and check key and val
-
-    return json::Result{"Not fully verified"};
-}
-
-json::Result verify_array(std::string_view line)
-{
-    return json::Result{ "verify_array() not implemented" };
-}
-
-json::Result verify_json(std::string_view line)
-{
-    if (line.size() == 0) return json::Result{std::string("Error verifying json. Empty string")};
-    json::Result res;
-    switch (line.front())
-    {
-        case '{':
-            // check bounds characters in verify_object
-            res = verify_object(line);
-            break;
-        case '[':
-            res = verify_array(line);
-            break;
-        default:
-            // other types as base data not supported. May add, but probably not
-            return json::Result(std::string("Error verifying json. Not object or array data: ") + std::string(line));
-    }
-
-    if (!res)
-    {
-        return json::Result("Error verifying object in json: ", res);
-    }
-    return res;
 }
