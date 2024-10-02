7f2f80d0fd709e21cda38798ace8f3dbaaa88372
Tristan Naess <tristannaess@gmail.com>
2024-10-02 12:31:10 -0700

    added todo throws as bodies of json and accessor member functions for testing

---
+++ include/json.hpp
@@ -0,0 +1,123 @@
+#ifndef __JSON_HPP__
+#define __JSON_HPP__
+
+#include <string>
+#include "error.hpp"
+
+
+namepsace json
+{
+    enum class Type
+    {
+        Object,
+        Array,
+        String,
+        Number,
+        Boolean,
+        Null
+    };
+
+    class json; // forward declaration to resolve circular references
+
+    class accessor
+    {
+        public:
+            // constructors
+            //accessor(json& parent, std::string::iterator front, std::string::iterator back);
+            accessor(std::string& data, std::string::iterator front, std::string::iterator back);
+            accessor(accessor&) = delete;
+            accessor(accessor&&) = delete;
+
+            // object data
+            std::vector<std::string> keys();        // json::wrong_type
+            bool contains(const std::string& key);  // json::wrong_type
+            bool contains(const char* key);         // json::wrong_type
+
+            // array data
+            std::size_t size(); // json::wrong_type
+
+            // general
+            Type type() noexcept;
+
+            // write operators; throw json::wrong_type
+            accessor& operator=(const std::string& str);
+            accessor& operator=(const char* str);
+            accessor& operator=(long double num);
+            accessor& operator=(double num);
+            accessor& operator=(float num);
+            accessor& operator=(unsigned long long int num);
+            accessor& operator=(unsigned long int num);
+            accessor& operator=(unsigned int num);
+            accessor& operator=(unsigned char num);
+            accessor& operator=(long long int num);
+            accessor& operator=(long int num);
+            accessor& operator=(int num);
+            accessor& operator=(char num);
+            accessor& operator=(bool boolean);
+
+            // read operators; throw json::wrong_type
+            operator std::string();
+            operator long double();
+            operator double();
+            operator float();
+            operator unsigned long long int();
+            operator unsigned long int();
+            operator unsigned int();
+            operator unsigned char();
+            operator long long int();
+            operator long int();
+            operator int();
+            operator char();
+            operator bool();
+
+            // subscript operators; throw json::out_of_range, json::wrong_type
+            accessor operator[](const std::string& key);
+            accessor operator[](const char* key);
+            accessor operator[](std::size_t index);
+
+        private:
+            //json& m_parent_json;
+            std::string& m_data; // see if this is enough, or if I need the json&
+            std::string::iterator m_front;
+            std::string::iterator m_back;
+            Type m_type;
+    };
+
+
+    class json
+    {
+        public:
+            // constructors
+            json();
+            json(const std::string& data);
+            json(const char* data);
+
+            // object data
+            std::vector<std::string> keys();        // json::wrong_type
+            bool contains(const std::string& key);  // json::wrong_type
+            bool contains(const char* key);         // json::wrong_type
+
+            // array data
+            std::size_t size(); // json::wrong_type
+
+            // general
+            Type type() noexcept;
+
+            // access
+            accessor operator[](const std::string& key);    // json::wrong_type, json::out_of_range
+            accessor operator[](const char* key);           // json::wrong_type, json::out_or_range
+            accessor operator[](std::size_t index);         // json::wrong_type, json::out_of_range
+
+            // once iterators are added
+            // iterator begin();
+            // iterator end();
+
+        private:
+            std::string m_data;
+            Type m_type;
+
+            friend class accessor;
+    };
+}
+
+#endif//__JSON_HPP__
---
+++ source/json.cpp
@@ -0,0 +1,261 @@
+#include "json.hpp"
+
+namespace json
+{
+
+    // -----------------------------------
+    //  json::json
+    // -----------------------------------
+
+    json::json()
+    {
+        throw todo{"TODO: json::json()"};
+    }
+
+    json::json(const std::string& key)
+    {
+        throw todo{"TODO: json::json(const std::string&)"};
+    }
+
+    json::json(const char* key)
+    {
+        throw todo{"TODO: json::json(const char*)"};
+    }
+
+    std::vector<std::string> json::keys() // json::wrong_type
+    {
+        throw todo{"TODO: json::keys()"};
+    }
+
+    bool json::contains(const std::string& key) // json::wrong_type
+    {
+        throw todo{"TODO: json::contains(const std::string&)"};
+    }
+
+    bool json::contains(const char* key) // json::wrong_type
+    {
+        throw todo{"TODO: json::contains(const char*)"};
+    }
+
+    std::size_t json::size() // json::wrong_type
+    {
+        throw todo{"TODO: json::size()"};
+    }
+
+    Type json::type() noexcept
+    {
+        return Type::Null; // would throw todo, but marked noexcept for later
+    }
+
+    accessor json::operator[](const std::string& key) // json::wrong_type, json::out_of_range
+    {
+        throw todo{"TODO: json::operator[](const std::string&)"};
+    }
+
+    accessor json::operator[](const char* key) // json::wrong_type, json::out_of_range
+    {
+        throw todo{"TODO: json::operator[](const char*)"};
+    }
+
+    accessor json::operator[](std::size_t index) // json::wrong_type, json::out_of_range
+    {
+        throw todo{"TODO: json::operator[](std::size_t)"};
+    }
+
+    // iterator json::begin()
+
+    // iterator json::end()
+
+
+    // -----------------------------------
+    //  json::accessor
+    // -----------------------------------
+
+    accessor(std::string& data, std::string::iterator front, std::string::iterator back)
+    {
+        throw todo{"TODO: accessor(std::string&, std::string::iterator, std::string::iterator)"};
+    }
+
+    std::vector<std::string> accessor::keys()       // json::wrong_type
+    {
+        throw todo{"TODO: accessor::keys()"};
+    }
+
+    bool accessor::contains(const std::string& key) // json::wrong_type
+    {
+        throw todo{"TODO: accessor::contains(const std::string&)"};
+    }
+
+    bool accessor::contains(const char* key)        // json::wrong_type
+    {
+        throw todo{"TODO: accessor::contains(const char*)"};
+    }
+
+    // array data
+    std::size_t accessor::size() // json::wrong_type
+    {
+        throw todo{"TODO: accessor::size()"};
+    }
+
+    // general
+    Type accessor::type() noexcept
+    {
+        return Type::Null;
+    }
+
+
+    // write operators; throw json::wrong_type
+    accessor& accessor::operator=(const std::string& str)
+    {
+        throw todo{"TODO: accessor::operator=(const std::string&)"};
+    }
+
+    accessor& accessor::operator=(const char* str)
+    {
+        throw todo{"TODO: accessor::operator=(const char*)"};
+    }
+
+    accessor& accessor::operator=(long double num)
+    {
+        throw todo{"TODO: accessor::operator=(long dnum)"};
+    }
+
+    accessor& accessor::operator=(double num)
+    {
+        throw todo{"TODO: accessor::operator=(dnum)"};
+    }
+
+    accessor& accessor::operator=(float num)
+    {
+        throw todo{"TODO: accessor::operator=(fnum)"};
+    }
+
+    accessor& accessor::operator=(unsigned long long int num)
+    {
+        throw todo{"TODO: accessor::operator=(unsigned long long inum)"};
+    }
+
+    accessor& accessor::operator=(unsigned long int num)
+    {
+        throw todo{"TODO: accessor::operator=(unsigned long inum)"};
+    }
+
+    accessor& accessor::operator=(unsigned int num)
+    {
+        throw todo{"TODO: accessor::operator=(unsigned inum)"};
+    }
+
+    accessor& accessor::operator=(unsigned char num)
+    {
+        throw todo{"TODO: accessor::operator=(unsigned cnum)"};
+    }
+
+    accessor& accessor::operator=(long long int num)
+    {
+        throw todo{"TODO: accessor::operator=(long long inum)"};
+    }
+
+    accessor& accessor::operator=(long int num)
+    {
+        throw todo{"TODO: accessor::operator=(long inum)"};
+    }
+
+    accessor& accessor::operator=(int num)
+    {
+        throw todo{"TODO: accessor::operator=(inum)"};
+    }
+
+    accessor& accessor::operator=(char num)
+    {
+        throw todo{"TODO: accessor::operator=(cnum)"};
+    }
+
+    accessor& accessor::operator=(bool boolean)
+    {
+        throw todo{"TODO: accessor::operator=(bboolean)"};
+    }
+
+
+    // read operators; throw json::wrong_type
+    accessor::operator std::string()
+    {
+        throw todo{"TODO: accessor::operator std::string()"};
+    }
+
+    accessor::operator long double()
+    {
+        throw todo{"TODO: accessor::operator long double()"};
+    }
+
+    accessor::operator double()
+    {
+        throw todo{"TODO: accessor::operator double()"};
+    }
+
+    accessor::operator float()
+    {
+        throw todo{"TODO: accessor::operator float()"};
+    }
+
+    accessor::operator unsigned long long int()
+    {
+        throw todo{"TODO: accessor::operator unsigned long long int()"};
+    }
+
+    accessor::operator unsigned long int()
+    {
+        throw todo{"TODO: accessor::operator unsigned long int()"};
+    }
+
+    accessor::operator unsigned int()
+    {
+        throw todo{"TODO: accessor::operator unsigned int()"};
+    }
+
+    accessor::operator unsigned char()
+    {
+        throw todo{"TODO: accessor::operator unsigned char()"};
+    }
+
+    accessor::operator long long int()
+    {
+        throw todo{"TODO: accessor::operator long long int()"};
+    }
+
+    accessor::operator long int()
+    {
+        throw todo{"TODO: accessor::operator long int()"};
+    }
+
+    accessor::operator int()
+    {
+        throw todo{"TODO: accessor::operator int()"};
+    }
+
+    accessor::operator char()
+    {
+        throw todo{"TODO: accessor::operator char()"};
+    }
+
+    accessor::operator bool()
+    {
+        throw todo{"TODO: accessor::operator bool()"};
+    }
+
+
+    // subscript operators; throw json::out_of_range, json::wrong_type
+    accessor accessor::operator[](const std::string& key)
+    {
+        throw todo{"TODO: accessor::operator[](const std::string&)"};
+    }
+
+    accessor accessor::operator[](const char* key)
+    {
+        throw todo{"TODO: accessor::operator[](const char*)"};
+    }
+    
+    accessor accessor::operator[](std::size_t index)
+    {
+        throw todo{"TODO: accessor::operator[](std::size_t)"};
+    }
+}
