d1d2cf1442455a52dd5ddbb54bd302baf0b6138c
Tristan Naess <tristannaess@gmail.com>
2025-02-08 20:13:12 -0800

    Added implementation for verify_number(), changed order of function declarations in verification.*

--- include/verification.hpp
+++ include/verification.hpp
@@ -6,20 +6,22 @@
 
 // I'm just going to use const std::string& and bool for now. I'll change to string_view after I get it working and add a mutable version
 // does not expect whitespace to be removed. Actually needs to be called before white space can be removed
+
+//  There's a lot of copying going on here in the recursive calls, change to string_view once working
 bool verify_json(const std::string& line);
 
 bool verify_object(const std::string& line);
 bool verify_array(const std::string& line);
-bool verify_number(const std::string& line);
 bool verify_string(const std::string& line);
+bool verify_number(const std::string& line);
 bool verify_bool(const std::string& line);
 bool verify_null(const std::string& line);
 
 // checks type of verified json, 
 bool is_object(const std::string& line);
 bool is_array(const std::string& line);
-bool is_number(const std::string& line);
 bool is_string(const std::string& line);
+bool is_number(const std::string& line);
 bool is_bool(const std::string& line);
 bool is_null(const std::string& line);
 
--- source/verification.cpp
+++ source/verification.cpp
@@ -27,24 +27,147 @@ bool verify_array(const std::string& line)
     throw todo("TODO: bool verify_array(const std::string&)");
 }
 
-bool verify_number(const std::string& line)
-{
-    throw todo("TODO: bool verify_number(const std::string&)");
-}
-
 bool verify_string(const std::string& line)
 {
     throw todo("TODO: bool verify_string(const std::string&)");
 }
 
+bool verify_number(const std::string& line)
+{
+    enum class State { Start, Int, Neg, Zero, Dec, ExpSign, Exp } state = State::Start;
+
+    // works because non-ascii are illegal here
+    for (auto c : line)
+    {
+        switch (state)
+        {
+            case State::Start:
+                switch (c)
+                {
+                    case '0': state = State::Zero; break;
+                    case '-': state = State::Neg; break;
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9': state = State::Int; break;
+                    default: return false;
+                }
+                break;
+            case State::Int:
+                switch (c)
+                {
+                    case '.': state = State::Dec; break;
+                    case 'e':
+                    case 'E': state = State::ExpSign; break;
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9': /*state = State::Int;*/ break;
+                    default: return false;
+
+                }
+                break;
+            case State::Neg:
+                switch (c)
+                {
+                    case '0': state = State::Zero; break;
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9': state = State::Int; break;
+                    default: return false;
+                }
+                break;
+            case State::Zero:
+                switch (c)
+                {
+                    case '.': state = State::Dec; break;
+                    case 'e':
+                    case 'E': state = State::ExpSign; break;
+                    default: return false;
+                }
+                break;
+            case State::Dec:
+                switch (c)
+                {
+                    case 'e':
+                    case 'E': state = State::ExpSign; break;
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9': /*state = State::Dec;*/ break;
+                    default: return false;
+                }
+                break;
+            case State::ExpSign:
+                switch (c)
+                {
+                    case '+':
+                    case '-':
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9': state = State::Exp; break;
+                    default: return false;
+                }
+                break;
+            case State::Exp:
+                switch (c)
+                {
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9': /*state = State::Exp;*/ break;
+                    default: return false;
+                }
+                break;
+        }
+    }
+    return state == State::Zero || state == State::Int || state == State::Dec || state == State::Exp;
+}
+
 bool verify_bool(const std::string& line)
 {
-    throw todo("TODO: bool verify_bool(const std::string&)");
+    return line == "true" || line == "false";
 }
 
 bool verify_null(const std::string& line)
 {
-    throw todo("TODO: bool verify_null(const std::string&)");
+    return line == "null";
 }
 
 
@@ -60,14 +183,14 @@ bool is_array(const std::string& line)
     return line[0] == '[';
 }
 
-bool is_number(const std::string& line)
-{
-    return (line[0] == '-' || (line[0] >= '0' && line[0] <= '9'));
-}
-
 bool is_string(const std::string& line)
 {
     return line[0] == '"';
+}
+
+bool is_number(const std::string& line)
+{
+    return (line[0] == '-' || (line[0] >= '0' && line[0] <= '9'));
 }
 
 bool is_bool(const std::string& line)
