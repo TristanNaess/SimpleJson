1527cfb619415d3a9574c89527209bd0ab01b0d3
Tristan Naess <tristannaess@gmail.com>
2025-03-29 14:41:23 -0700

    Added tests for conversion from string to data, to and from long double, double, float. No implementations of tested functions

--- source/test/conversion.cpp
+++ source/test/conversion.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "conversion.hpp"
+#include "error.hpp"
 
 TEST(Conversion, String)
 {
@@ -16,39 +17,170 @@ TEST(Conversion, String)
     EXPECT_EQ(conversion::to_data("This line contains unicode characters: \u00ab \u01da \uad2f"), std::string("This line contains unicode characters: \u00ab \u01da \uad2f")) << "Failed to convert string containing unicode characters to internal representation";
 }
 
-/*
 TEST(Conversion, LongDouble)
 {
+    long double epsilon = std::numeric_limits<long double>::epsilon();
     // To long double passing
+    EXPECT_NEAR(conversion::to_ldouble("0"), 0L, epsilon) << "Failed to convert 0 from internal representation to long double";
+    EXPECT_NEAR(conversion::to_ldouble("-0"), 0L, epsilon) << "Failed to convert -0 from internal representation to long double";
+    EXPECT_NEAR(conversion::to_ldouble("12345"), 12345L, epsilon) << "Failed to convert positive integer from internal representation to long double";
+    EXPECT_NEAR(conversion::to_ldouble("-9876"), -9876L, epsilon) << "Failed to convert negative integer from internal representation to long double";
+    EXPECT_NEAR(conversion::to_ldouble(".341"), .341L, epsilon) << "Failed to convert positive decimal < 1 from internal representation to long double";
+    EXPECT_NEAR(conversion::to_ldouble("-.9476"), -.9476L, epsilon) << "Failed to convert negative decimal > -1 from internal representation to long double";
+    EXPECT_NEAR(conversion::to_ldouble("3.14159"), 3.14159L, epsilon) << "Failed to convert positive decimal > 1 from internal representation to long double";
+    EXPECT_NEAR(conversion::to_ldouble("-54.3543"), -54.3543L, epsilon) << "Failed to convert negative decimal < -1 from internal representation to long double";
+    EXPECT_NEAR(conversion::to_ldouble("123e89"), 123e89L, epsilon) << "Failed to convert positive integer and positive exponent from internal representation to long double";
+    EXPECT_NEAR(conversion::to_ldouble("652e-12"), 652e-12L, epsilon) << "Failed to convert positive integer and negative exponent from internal representation to long double";
+    EXPECT_NEAR(conversion::to_ldouble("-54e763"), -54e763L, epsilon) << "Failed to convert negative integer and positive exponent from internal representation to long double";
+    EXPECT_NEAR(conversion::to_ldouble("-890e-5"), -890e-5L, epsilon) << "Failed to convert negative integer and negative exponent from internal representation to long double";
+    EXPECT_NEAR(conversion::to_ldouble("6.0221408e+23"), 6.0221408e+23L, epsilon) << "Failed to convert positive decimal and positive exponent from internal representation to long double";
+    EXPECT_NEAR(conversion::to_ldouble("9.1093837e-31"), 9.1093837e-31L, epsilon) << "Failed to convert positive decimal and negative exponent from internal representation to long double";
+    EXPECT_NEAR(conversion::to_ldouble("-147.142e43"), -147.142e43L, epsilon) << "Failed to convert negative decimal and positive exponent from internal representation to long double";
+    EXPECT_NEAR(conversion::to_ldouble("-432.42e-5"), -432.42e-5L, epsilon) << "Failed to convert negative decimal and negative exponent from internal representation to long double";
+    
+    long double max = std::numeric_limits<long double>::max();
+    std::string max_str = std::to_string(max);
+    long double min = std::numeric_limits<long double>::lowest();
+    std::string min_str = std::to_string(min);
+    EXPECT_NEAR(conversion::to_ldouble(max_str), max, epsilon) << "Failed to convert max long double from internal representation to long double";
+    EXPECT_NEAR(conversion::to_ldouble(min_str), min, epsilon) << "Failed to convert min long double from internal representation to long double";
     
     // From long double passing
-
+    EXPECT_EQ(conversion::to_data(0L), "0") << "Failed to convert 0 from long double to internal representation";
+    EXPECT_EQ(conversion::to_data(0L), "-0") << "Failed to convert -0 from long double to internal representation";
+    EXPECT_EQ(conversion::to_data(12345L), "12345") << "Failed to convert positive integer from long double to internal representation";
+    EXPECT_EQ(conversion::to_data(-9876L), "-9876") << "Failed to convert negative integer from long double to internal representation";
+    EXPECT_EQ(conversion::to_data(.341L), ".341") << "Failed to convert positive decimal < 1 from long double to internal representation";
+    EXPECT_EQ(conversion::to_data(-.9476L), "-.9476") << "Failed to convert negative decimal > -1 from long double to internal representation";
+    EXPECT_EQ(conversion::to_data(3.14159L), "3.14159") << "Failed to convert positive decimal > 1 from long double to internal representation";
+    EXPECT_EQ(conversion::to_data(-54.3543L), "-54.3543") << "Failed to convert negative decimal < -1 from long double to internal representation";
+    EXPECT_EQ(conversion::to_data(123e89L), "123e89") << "Failed to convert positive integer and positive exponent from long double to internal representation";
+    EXPECT_EQ(conversion::to_data(652e-12L), "652e-12") << "Failed to convert positive integer and negative exponent from long double to internal representation";
+    EXPECT_EQ(conversion::to_data(-54e763L), "-54e763") << "Failed to convert negative integer and positive exponent from long double to internal representation";
+    EXPECT_EQ(conversion::to_data(-890e-5L), "-890e-5") << "Failed to convert negative integer and negative exponent from long double to internal representation";
+    EXPECT_EQ(conversion::to_data(6.0221408e+23L), "6.0221408e+23") << "Failed to convert positive decimal and positive exponent from long double to internal representation";
+    EXPECT_EQ(conversion::to_data(9.1093837e-31L), "9.1093837e-31") << "Failed to convert positive decimal and negative exponent from long double to internal representation";
+    EXPECT_EQ(conversion::to_data(-147.142e43L), "-147.142e43") << "Failed to convert negative decimal and positive exponent from long double to internal representation";
+    EXPECT_EQ(conversion::to_data(-432.42e-5L), "-432.42e-5") << "Failed to convert negative decimal and negative exponent from long double to internal representation";
+    
+    EXPECT_EQ(conversion::to_data(max), max_str) << "Failed to convert max long double from long double to internal representation";
+    EXPECT_EQ(conversion::to_data(min), min_str) << "Failed to convert min long double from long double to internal representation";
 }
 
 TEST(Conversion, Double)
 {
+    double epsilon = std::numeric_limits<double>::epsilon();
     // To double passing
+    EXPECT_NEAR(conversion::to_double("0"), 0L, epsilon) << "Failed to convert 0 from internal representation to double";
+    EXPECT_NEAR(conversion::to_double("-0"), 0L, epsilon) << "Failed to convert -0 from internal representation to double";
+    EXPECT_NEAR(conversion::to_double("12345"), 12345L, epsilon) << "Failed to convert positive integer from internal representation to double";
+    EXPECT_NEAR(conversion::to_double("-9876"), -9876L, epsilon) << "Failed to convert negative integer from internal representation to double";
+    EXPECT_NEAR(conversion::to_double(".341"), .341L, epsilon) << "Failed to convert positive decimal < 1 from internal representation to double";
+    EXPECT_NEAR(conversion::to_double("-.9476"), -.9476L, epsilon) << "Failed to convert negative decimal > -1 from internal representation to double";
+    EXPECT_NEAR(conversion::to_double("3.14159"), 3.14159L, epsilon) << "Failed to convert positive decimal > 1 from internal representation to double";
+    EXPECT_NEAR(conversion::to_double("-54.3543"), -54.3543L, epsilon) << "Failed to convert negative decimal < -1 from internal representation to double";
+    EXPECT_NEAR(conversion::to_double("123e89"), 123e89L, epsilon) << "Failed to convert positive integer and positive exponent from internal representation to double";
+    EXPECT_NEAR(conversion::to_double("652e-12"), 652e-12L, epsilon) << "Failed to convert positive integer and negative exponent from internal representation to double";
+    EXPECT_NEAR(conversion::to_double("-54e763"), -54e763L, epsilon) << "Failed to convert negative integer and positive exponent from internal representation to double";
+    EXPECT_NEAR(conversion::to_double("-890e-5"), -890e-5L, epsilon) << "Failed to convert negative integer and negative exponent from internal representation to double";
+    EXPECT_NEAR(conversion::to_double("6.0221408e+23"), 6.0221408e+23L, epsilon) << "Failed to convert positive decimal and positive exponent from internal representation to double";
+    EXPECT_NEAR(conversion::to_double("9.1093837e-31"), 9.1093837e-31L, epsilon) << "Failed to convert positive decimal and negative exponent from internal representation to double";
+    EXPECT_NEAR(conversion::to_double("-147.142e43"), -147.142e43L, epsilon) << "Failed to convert negative decimal and positive exponent from internal representation to double";
+    EXPECT_NEAR(conversion::to_double("-432.42e-5"), -432.42e-5L, epsilon) << "Failed to convert negative decimal and negative exponent from internal representation to double";
+    
+    double max = std::numeric_limits<double>::max();
+    std::string max_str = std::to_string(max);
+    double min = std::numeric_limits<double>::lowest();
+    std::string min_str = std::to_string(min);
+    EXPECT_NEAR(conversion::to_double(max_str), max, epsilon) << "Failed to convert max double from internal representation to double";
+    EXPECT_NEAR(conversion::to_double(min_str), min, epsilon) << "Failed to convert min double from internal representation to double";
 
     // To double failing
+    EXPECT_THROW(conversion::to_double(std::to_string(max + 1000)), json::out_of_range) << "Failed to throw json::out_of_range error for value greater than possible to store in double";
+    EXPECT_THROW(conversion::to_double(std::to_string(min - 1000)), json::out_of_range) << "Failed to throw json::out_of_range error for negative value of greater magnitude than possible to store in double";
 
     // From double passing
+    EXPECT_EQ(conversion::to_data(0L), "0") << "Failed to convert 0 from double to internal representation";
+    EXPECT_EQ(conversion::to_data(0L), "-0") << "Failed to convert -0 from double to internal representation";
+    EXPECT_EQ(conversion::to_data(12345L), "12345") << "Failed to convert positive integer from double to internal representation";
+    EXPECT_EQ(conversion::to_data(-9876L), "-9876") << "Failed to convert negative integer from double to internal representation";
+    EXPECT_EQ(conversion::to_data(.341L), ".341") << "Failed to convert positive decimal < 1 from double to internal representation";
+    EXPECT_EQ(conversion::to_data(-.9476L), "-.9476") << "Failed to convert negative decimal > -1 from double to internal representation";
+    EXPECT_EQ(conversion::to_data(3.14159L), "3.14159") << "Failed to convert positive decimal > 1 from double to internal representation";
+    EXPECT_EQ(conversion::to_data(-54.3543L), "-54.3543") << "Failed to convert negative decimal < -1 from double to internal representation";
+    EXPECT_EQ(conversion::to_data(123e89L), "123e89") << "Failed to convert positive integer and positive exponent from double to internal representation";
+    EXPECT_EQ(conversion::to_data(652e-12L), "652e-12") << "Failed to convert positive integer and negative exponent from double to internal representation";
+    EXPECT_EQ(conversion::to_data(-54e763L), "-54e763") << "Failed to convert negative integer and positive exponent from double to internal representation";
+    EXPECT_EQ(conversion::to_data(-890e-5L), "-890e-5") << "Failed to convert negative integer and negative exponent from double to internal representation";
+    EXPECT_EQ(conversion::to_data(6.0221408e+23L), "6.0221408e+23") << "Failed to convert positive decimal and positive exponent from double to internal representation";
+    EXPECT_EQ(conversion::to_data(9.1093837e-31L), "9.1093837e-31") << "Failed to convert positive decimal and negative exponent from double to internal representation";
+    EXPECT_EQ(conversion::to_data(-147.142e43L), "-147.142e43") << "Failed to convert negative decimal and positive exponent from double to internal representation";
+    EXPECT_EQ(conversion::to_data(-432.42e-5L), "-432.42e-5") << "Failed to convert negative decimal and negative exponent from double to internal representation";
+    
+    EXPECT_EQ(conversion::to_data(max), max_str) << "Failed to convert max double from double to internal representation";
+    EXPECT_EQ(conversion::to_data(min), min_str) << "Failed to convert min double from double to internal representation";
 
-    // From double failing
-
-}
+    // From double can't call conversion::to_data(double) with long double arguement, will call long double version. Error will occur at assignment
+} 
 
 TEST(Conversion, Float)
 {
+    float epsilon = std::numeric_limits<float>::epsilon();
     // To float passing
+    EXPECT_NEAR(conversion::to_float("0"), 0L, epsilon) << "Failed to convert 0 from internal representation to float";
+    EXPECT_NEAR(conversion::to_float("-0"), 0L, epsilon) << "Failed to convert -0 from internal representation to float";
+    EXPECT_NEAR(conversion::to_float("12345"), 12345L, epsilon) << "Failed to convert positive integer from internal representation to float";
+    EXPECT_NEAR(conversion::to_float("-9876"), -9876L, epsilon) << "Failed to convert negative integer from internal representation to float";
+    EXPECT_NEAR(conversion::to_float(".341"), .341L, epsilon) << "Failed to convert positive decimal < 1 from internal representation to float";
+    EXPECT_NEAR(conversion::to_float("-.9476"), -.9476L, epsilon) << "Failed to convert negative decimal > -1 from internal representation to float";
+    EXPECT_NEAR(conversion::to_float("3.14159"), 3.14159L, epsilon) << "Failed to convert positive decimal > 1 from internal representation to float";
+    EXPECT_NEAR(conversion::to_float("-54.3543"), -54.3543L, epsilon) << "Failed to convert negative decimal < -1 from internal representation to float";
+    EXPECT_NEAR(conversion::to_float("123e89"), 123e89L, epsilon) << "Failed to convert positive integer and positive exponent from internal representation to float";
+    EXPECT_NEAR(conversion::to_float("652e-12"), 652e-12L, epsilon) << "Failed to convert positive integer and negative exponent from internal representation to float";
+    EXPECT_NEAR(conversion::to_float("-54e763"), -54e763L, epsilon) << "Failed to convert negative integer and positive exponent from internal representation to float";
+    EXPECT_NEAR(conversion::to_float("-890e-5"), -890e-5L, epsilon) << "Failed to convert negative integer and negative exponent from internal representation to float";
+    EXPECT_NEAR(conversion::to_float("6.0221408e+23"), 6.0221408e+23L, epsilon) << "Failed to convert positive decimal and positive exponent from internal representation to float";
+    EXPECT_NEAR(conversion::to_float("9.1093837e-31"), 9.1093837e-31L, epsilon) << "Failed to convert positive decimal and negative exponent from internal representation to float";
+    EXPECT_NEAR(conversion::to_float("-147.142e43"), -147.142e43L, epsilon) << "Failed to convert negative decimal and positive exponent from internal representation to float";
+    EXPECT_NEAR(conversion::to_float("-432.42e-5"), -432.42e-5L, epsilon) << "Failed to convert negative decimal and negative exponent from internal representation to float";
+    
+    float max = std::numeric_limits<float>::max();
+    std::string max_str = std::to_string(max);
+    float min = std::numeric_limits<float>::lowest();
+    std::string min_str = std::to_string(min);
+    EXPECT_NEAR(conversion::to_float(max_str), max, epsilon) << "Failed to convert max float from internal representation to float";
+    EXPECT_NEAR(conversion::to_float(min_str), min, epsilon) << "Failed to convert min float from internal representation to float";
     
     // To float failing
+    EXPECT_THROW(conversion::to_float(std::to_string(max + 1000)), json::out_of_range) << "Failed to throw json::out_of_range error for value greater than possible to store in float";
+    EXPECT_THROW(conversion::to_float(std::to_string(min - 1000)), json::out_of_range) << "Failed to throw json::out_of_range error for negative value of greater magnitude than possible to store in float";
 
-    // To float passing
+    // From float passing
+    EXPECT_EQ(conversion::to_data(0L), "0") << "Failed to convert 0 from float to internal representation";
+    EXPECT_EQ(conversion::to_data(0L), "-0") << "Failed to convert -0 from float to internal representation";
+    EXPECT_EQ(conversion::to_data(12345L), "12345") << "Failed to convert positive integer from float to internal representation";
+    EXPECT_EQ(conversion::to_data(-9876L), "-9876") << "Failed to convert negative integer from float to internal representation";
+    EXPECT_EQ(conversion::to_data(.341L), ".341") << "Failed to convert positive decimal < 1 from float to internal representation";
+    EXPECT_EQ(conversion::to_data(-.9476L), "-.9476") << "Failed to convert negative decimal > -1 from float to internal representation";
+    EXPECT_EQ(conversion::to_data(3.14159L), "3.14159") << "Failed to convert positive decimal > 1 from float to internal representation";
+    EXPECT_EQ(conversion::to_data(-54.3543L), "-54.3543") << "Failed to convert negative decimal < -1 from float to internal representation";
+    EXPECT_EQ(conversion::to_data(123e89L), "123e89") << "Failed to convert positive integer and positive exponent from float to internal representation";
+    EXPECT_EQ(conversion::to_data(652e-12L), "652e-12") << "Failed to convert positive integer and negative exponent from float to internal representation";
+    EXPECT_EQ(conversion::to_data(-54e763L), "-54e763") << "Failed to convert negative integer and positive exponent from float to internal representation";
+    EXPECT_EQ(conversion::to_data(-890e-5L), "-890e-5") << "Failed to convert negative integer and negative exponent from float to internal representation";
+    EXPECT_EQ(conversion::to_data(6.0221408e+23L), "6.0221408e+23") << "Failed to convert positive decimal and positive exponent from float to internal representation";
+    EXPECT_EQ(conversion::to_data(9.1093837e-31L), "9.1093837e-31") << "Failed to convert positive decimal and negative exponent from float to internal representation";
+    EXPECT_EQ(conversion::to_data(-147.142e43L), "-147.142e43") << "Failed to convert negative decimal and positive exponent from float to internal representation";
+    EXPECT_EQ(conversion::to_data(-432.42e-5L), "-432.42e-5") << "Failed to convert negative decimal and negative exponent from float to internal representation";
+    
+    EXPECT_EQ(conversion::to_data(max), max_str) << "Failed to convert max float from float to internal representation";
+    EXPECT_EQ(conversion::to_data(min), min_str) << "Failed to convert min float from float to internal representation";
 
-    // To float failing
+    // From float can't call conversion::to_data(float) with double arguement, will call double version. Error will occur at assignment
 
 }
 
+/*
 TEST(Conversion, LongLongUnsigned)
 {
     // To long long unsigned int passing
